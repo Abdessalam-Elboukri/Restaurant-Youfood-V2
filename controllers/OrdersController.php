f164c6d5c6805a5bc15274146682286b49504e31
abdessalam-elboukri <abdessalamelboukri00@gmail.com>
2022-06-22 01:58:52 -0700

    add get commands plat let the user show the commands that ordered in specific date

--- controllers/OrdersController.php
+++ controllers/OrdersController.php
@@ -27,6 +27,18 @@ class OrdersController extends Controller
         return $this->render('plats-menu');
     }
     
-     
+    public function getCommands(Request $request){
+        $command = new OrdersModel();
+        if($request ->isPost())
+        {
+            $command->loadData($request->getBody());
+            $command ->getMyCommands($_POST['date_c'],$_SESSION['user_id'] );
+            $command->loadData($command->dataList);
+            // var_dump($command->dataList[0]); exit;
+            $params = [
+                'data1' => $command->dataList
+            ]; 
+            return $this->render('vos-plats', $params);
+        }
+    }
 }
-
