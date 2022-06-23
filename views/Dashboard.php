638a25742ab6a8fa86ea0aa567803c0504c4d8ae
aymane <aymanedarj7@gmail.com>
2022-06-23 19:12:02 +0100

    ADD some modification to the dashboard page

--- views/Dashboard.php
+++ views/Dashboard.php
@@ -1,12 +1,20 @@
 <main>
-    <div class="mini-navBar w-100  position-fixed border-bottom border-2 bg-white">
-        <ul class="ms-5 h-100 daysList list-unstyled d-flex gap-4 align-items-center">
-            <li class=" border-dark border-bottom border-2 fs-16">Lundi</li>
-            <li class="border-dark border-bottom border-2  fs-16">Mardi</li>
-            <li class="border-dark border-bottom border-2  fs-16">Mercredi</li>
-            <li class="border-dark border-bottom border-2 fs-16">Jeudi</li>
-            <li class="border-dark border-bottom border-2 fs-16">Vendredi</li>
-        </ul>
+    <div class=" mini-navBar border-bottom border-2 d-flex justify-content-between align-items-center">
+        <div class="d-flex">
+            <h6 class="fw-bold m-0 ps-3">Commandes de la journée</h6>
+        </div>
+
+
+        <div class="me-4 border bg-light  ps-2">
+            <form action="" method="POST" class="d-flex form-group">
+                <input type="date" class=" border-0 focus-0 text-green fw-bold bg-none form-control">
+                <button class="btn-green btn text-white " type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        </svg>
+                </button>
+            </form>
+        </div>
 
     </div>
     <!--Main Section-->
@@ -19,16 +27,14 @@
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center">
                                 <h6 class="fs-14 fw-bold ">Entrée</h6>
-                                <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                     class="" width="50px" alt="">
+                                <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" class="" width="50px" alt="">
                             </div>
                             <hr class="mt-0">
                         </div>
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center gap-3">
                                 <div>
-                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                         width="50px" alt="">
+                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" width="50px" alt="">
                                     <span class="fs-14 fw-bold ms-3">Salade verte</span>
                                 </div>
                                 <span class="fs-14 fw-bold text-green px-2 py-1">80</span>
@@ -38,8 +44,7 @@
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center">
                                 <div>
-                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                         width="50px" alt="">
+                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" width="50px" alt="">
                                     <span class="fs-14 fw-bold ms-3">Salade verte</span>
                                 </div>
                                 <span class="fs-14 fw-bold text-red text-center px-2 py-1">95</span>
@@ -49,8 +54,7 @@
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center">
                                 <div>
-                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                         width="50px" alt="">
+                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" width="50px" alt="">
                                     <span class="fs-14 fw-bold ms-3">Salade verte</span>
                                 </div>
                                 <span class="fs-14 fw-bold px-2 py-1">70</span>
@@ -61,8 +65,7 @@
                         <hr class="mt-0 mb-1">
                         <div class="d-flex justify-content-between align-items-center mt-2">
                             <span class="fs-13 ms-1">Quantité total</span>
-                            <span
-                                    class="btn_green_price text-green text-center fs-14 px-2 fw-bold py-1">120</span>
+                            <span class="btn_green_price text-green text-center fs-14 px-2 fw-bold py-1">120</span>
                         </div>
                     </div>
                 </div>
@@ -71,16 +74,14 @@
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center">
                                 <h6 class="fs-14 fw-bold ">Plat Principal</h6>
-                                <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                     class="" width="50px" alt="">
+                                <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" class="" width="50px" alt="">
                             </div>
                             <hr class="mt-0">
                         </div>
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center gap-3">
                                 <div>
-                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                         width="50px" alt="">
+                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" width="50px" alt="">
                                     <span class="fs-14 fw-bold ms-3">Poulet Mariné</span>
                                 </div>
                                 <span class="fs-14 fw-bold text-green px-2 py-1">80</span>
@@ -90,8 +91,7 @@
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center">
                                 <div>
-                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                         width="50px" alt="">
+                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" width="50px" alt="">
                                     <span class="fs-14 fw-bold ms-3">Poisson grillé</span>
                                 </div>
                                 <span class="fs-14 fw-bold text-red text-center px-2 py-1">95</span>
@@ -101,8 +101,7 @@
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center">
                                 <div>
-                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                         width="50px" alt="">
+                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" width="50px" alt="">
                                     <span class="fs-14 fw-bold ms-3">Autre</span>
                                 </div>
                                 <span class="fs-14 fw-bold px-2 py-1">70</span>
@@ -113,8 +112,7 @@
                         <hr class="mt-0 mb-1">
                         <div class="d-flex justify-content-between align-items-center mt-2">
                             <span class="fs-13 ms-1">Quantité total</span>
-                            <span
-                                    class="btn_green_price text-green text-center fs-14 px-2 fw-bold py-1">120</span>
+                            <span class="btn_green_price text-green text-center fs-14 px-2 fw-bold py-1">120</span>
                         </div>
                     </div>
                 </div>
@@ -123,16 +121,14 @@
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center">
                                 <h6 class="fs-14 fw-bold ">Entrée</h6>
-                                <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                     class="" width="50px" alt="">
+                                <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" class="" width="50px" alt="">
                             </div>
                             <hr class="mt-0">
                         </div>
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center gap-3">
                                 <div>
-                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                         width="50px" alt="">
+                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" width="50px" alt="">
                                     <span class="fs-14 fw-bold ms-3">Salade verte</span>
                                 </div>
                                 <span class="fs-14 fw-bold text-green px-2 py-1">80</span>
@@ -142,8 +138,7 @@
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center">
                                 <div>
-                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                         width="50px" alt="">
+                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" width="50px" alt="">
                                     <span class="fs-14 fw-bold ms-3">Salade verte</span>
                                 </div>
                                 <span class="fs-14 fw-bold text-red text-center px-2 py-1">95</span>
@@ -153,8 +148,7 @@
                         <div class="">
                             <div class="d-flex justify-content-between align-items-center">
                                 <div>
-                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png"
-                                         width="50px" alt="">
+                                    <img src="images/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png" width="50px" alt="">
                                     <span class="fs-14 fw-bold ms-3">Salade verte</span>
                                 </div>
                                 <span class="fs-14 fw-bold px-2 py-1">70</span>
@@ -165,8 +159,7 @@
                         <hr class="mt-0 mb-1">
                         <div class="d-flex justify-content-between align-items-center mt-2">
                             <span class="fs-13 ms-1 fw-bold">Quantité total</span>
-                            <span
-                                    class="btn_green_price text-green text-center fs-14 px-2 fw-bold py-1">120</span>
+                            <span class="btn_green_price text-green text-center fs-14 px-2 fw-bold py-1">120</span>
                         </div>
                     </div>
                 </div>
